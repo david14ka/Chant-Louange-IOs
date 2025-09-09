f580d453b29436e87af58cb2dc0971a4bc8cd4d3
14ka135 <david.kazad@hotmail.com>
2025-09-09 13:07:50 -0500

    Initial Commit

---
+++ ccantiques/ccantiquesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ccantiquesApp.swift
+//  ccantiques
+//
+//  Created by DAVID KAZAD on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ccantiquesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ccantiquesTests/ccantiquesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ccantiquesTests.swift
+//  ccantiquesTests
+//
+//  Created by DAVID KAZAD on 9/9/25.
+//
+
+import Testing
+@testable import ccantiques
+
+struct ccantiquesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
