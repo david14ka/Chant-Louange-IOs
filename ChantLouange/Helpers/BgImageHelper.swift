e5400244d83c5227d0706e246cb282da1e419fb0
14ka135 <david.kazad@hotmail.com>
2025-09-18 13:39:46 -0500

    Adding Backgound to views

---
+++ ChantLouange/Helpers/BgImageHelper.swift
@@ -0,0 +1,31 @@
+//
+//  BackgoundImageGradient.swift
+//  ChantLouange
+//
+//  Created by DAVID KAZAD on 9/18/25.
+//
+
+import SwiftUI
+
+struct BgImageGradient : View {
+    let image: String
+    
+    var body: some View {
+        return ZStack {
+            Image(image)
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+
+            LinearGradient(
+                gradient: Gradient(colors: [
+                    Color.black.opacity(0.6), // dark bottom
+                    Color.clear              // fade to transparent
+                ]),
+                startPoint: .bottom,
+                endPoint: .top
+            )
+            .ignoresSafeArea()
+        }
+    }
+}
