f7b005e3766fc92dcdbc0f3131370005d1ea4816
14ka135 <david.kazad@hotmail.com>
2025-09-22 12:42:20 -0500

    TabView added to MainView

---
+++ ChantLouange/BookMainView.swift
@@ -0,0 +1,7 @@
+//
+//  MainView.swift
+//  ChantLouange
+//
+//  Created by DAVID KAZAD on 9/19/25.
+//
+
---
+++ ChantLouange/TabInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  TabInfoView.swift
+//  ccantiques
+//
+//  Created by DAVID KAZAD on 9/22/25.
+//
+
+import SwiftUI
+
+struct TabInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabInfoView()
+}
